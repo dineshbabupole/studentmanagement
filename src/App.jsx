f9b7757a4b3dcfdd257f268108e239e98d9b61b1
dineshbabupole8 <dineshbabupole8@gmail.com>
2025-09-20 15:16:08 +0530

    message'

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import StudentForm from "./StudentForm";
 import StudentList from "./StudentList";
 import './assets/css/style.css'; 
 
-const API_URL = "http://studentmanagementbackend-production-08e7.up.railway.app/students";
+const API_URL = "https://studentmanagementbackend-production-08e7.up.railway.app/students";
 
 function App() {
   const [students, setStudents] = useState([]);
